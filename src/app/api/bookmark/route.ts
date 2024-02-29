cbc77309d7c38503d27b7d654f16b49809017d00
李云梦 <yunmeng.li@successchannel.com>
2024-02-29 16:49:21 +0800

    add bookmark snyc api

--- src/app/api/bookmark/route.ts
+++ src/app/api/bookmark/route.ts
@@ -1,7 +1,17 @@
+import { headers } from 'next/headers'
+import {getBookmarkData,updateBookmarkData} from "@/lib/api"
 
+export async function POST(request: Request, response: Response) {
+    const headersList = headers();
+    const API_TOKEN = headersList.get('API_TOKEN');
+    if(API_TOKEN != process.env.API_TOKEN){
+        return new Response('you have no right to call this!', {
+            status: 403
+          })
+    }
+    const body = await request.json();
+    console.log(body);
+    await updateBookmarkData(body);
 
-export async function POST(request: Request,response:Response) {
-    const str = request.body;
-    console.log(str);
-    return Response.json({"status":"ok"})
+    return Response.json({ "status": "ok" });
 }
