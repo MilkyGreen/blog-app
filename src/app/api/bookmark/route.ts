1fe35c9068010015177379bd800062029a8951a6
李云梦 <yunmeng.li@successchannel.com>
2024-02-29 18:00:03 +0800

    set cors

--- src/app/api/bookmark/route.ts
+++ src/app/api/bookmark/route.ts
@@ -16,10 +16,5 @@ export async function POST(request: Request, response: Response) {
 
     return new Response('done!', {
         status: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
     })
 }
