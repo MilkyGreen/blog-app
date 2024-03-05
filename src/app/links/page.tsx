4e0bd9829ed931e22cafc35bc63bdca5d83f8bb3
李云梦 <yunmeng.li@successchannel.com>
2024-03-05 17:26:39 +0800

    test

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -3,6 +3,8 @@ import {Record,Bookmark} from "@/app/interfaces/bookmarks";
 import { getBookmarkData } from "@/app/lib/api";
 import LeftTab from "@/app/links/tabs"
 
+export const dynamic = 'force-dynamic'
+
 export default async function Index() {
     const bk = await getBookmarkData();
     console.log("查询到的书签数据："+JSON.stringify(bk.children))
