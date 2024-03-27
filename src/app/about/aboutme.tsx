c502abb6b05024995b20c139adb7a04ab238a08d
李云梦 <yunmeng.li@successchannel.com>
2024-03-27 15:18:58 +0800

    beautify

--- src/app/about/aboutme.tsx
+++ src/app/about/aboutme.tsx
@@ -1,6 +1,6 @@
 export default function Content() {
   return (
-    <div className="grid grid-cols-1 gap-4">
+    <div className="grid grid-cols-1 gap-4 text-gray-600 text-lg font-mono">
       <div>
         As a Software Engineer with 10 years of experience in back-end development, I specialize in designing and implementing web servers and middlewares using Java(8,11). Also well-versed in working with distributed systems and optimizing performance. In spare time, I like to study things that interest me, such as interpreters, blockchain, and so on.
       </div>
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,11 +2,20 @@ import Content from "@/app/about/aboutme";
 
 export default function Index() {
     return (
-        <div className="grid grid-cols-12 gap-4 mx-40">
-            <div className="flex justify-center col-span-3 items-center">
-                Li Yunmeng
+        <div className="grid grid-cols-12">
+            <div className="justify-center col-span-3 items-center grid grid-cols-1 ml-20">
+                <div className="">Li Yunmeng</div>
+                <div>
+                    liyunmeng2017@gmail.com
+                </div>
+                <div className="flex">
+                    <a href="https://github.com/MilkyGreen" target="_blank"  className="mr-2 text-blue-400 underline">Github</a>
+                    <a href="https://www.linkedin.com/in/li-yunmeng-2a040590/" target="_blank" className="mr-2  text-blue-400 underline">Linkedin</a>
+                    <a href="https://leetcode.cn/u/milkygreen/" target="_blank" className="mr-2 text-blue-400 underline">Leetcode</a>
+                </div>
+                <div className=" h-10"></div>
             </div>
-            <div className="col-span-7 font-extralight">
+            <div className="col-span-7">
                 <Content />
             </div>
             
